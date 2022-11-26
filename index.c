cd4abe6d01d607357e3601a31a616d763a332038
ramsceoskull <ramses.social0@gmail.com>
2022-11-26 17:05:51 -0600

    Se añadió el menú

--- index.c
+++ index.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
 
-int main ()
+int main()
 {
-    return 0; 
+   int choice, value1, value2;
+   do
+   {
+      printf("Welcome to our Calculator in C\n");
+      printf("1. Addition\n");
+      printf("2. Subtraction\n");
+      printf("3. Multiplication\n");
+      printf("4. Division\n");
+      printf("0. Exit\n");
+      printf("Enter your choice: ");
+      scanf("%i", &choice);
+      printf("Please enter the first value: ");
+      scanf("%i", &value1);
+      printf("Please enter the second value: ");
+      scanf("%i", &value2);
+      switch(choice)
+      {
+         case 1:
+            printf("Addition\n");
+            break;
+         case 2:
+            printf("Subtraction\n");
+            break;
+         case 3:
+            printf("Multiplication\n");
+            break;
+         case 4:
+            printf("Division\n");
+            break;
+         case 0:
+            printf("Thank you for using our calculator\n");
+            break;
+         default:
+            printf("Invalid choice");
+      }
+      printf("\n\n");
+   } while (choice != 0);
+   return 0;
 }
